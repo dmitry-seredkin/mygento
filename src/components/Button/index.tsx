8ab5794ebbd22143ab21a8509decadb682826d72
seroydkin <dimsf97@gmail.com>
2020-11-14 17:24:39 +0300

    add scss support

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,14 @@
 import React, { FC } from 'react'
 
+import styles from './Button.module.scss'
+
 type TOuterProps = {
   className?: string
 }
 type TProps = TOuterProps
 
 const Button: FC<TProps> = ({  }) => (
-  <button>check</button>
+  <button className={styles.button}>check</button>
 )
 
 export default Button
